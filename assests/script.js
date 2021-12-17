ba933aa4b66c05c86deeac9c15ce4c463c868bd6
Bertokeys1 <abarrientos93.ab@gmail.com>
2021-12-16 21:04:11 -0800

    updated html and js file

--- assests/script.js
+++ assests/script.js
@@ -10,6 +10,7 @@
     // Get the vity from the button's data attribute
 
 
+
 var searchBtn = $("#search-btn")
 
 searchBtn.on("click", cityName);
@@ -53,7 +54,7 @@ function geoData(cityName) {
 // Fetch the one call weather data
 function oneCall(lat,lon) {
 
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=5f0de267b85fd710a6d2a256aad58f56`;
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5f0de267b85fd710a6d2a256aad58f56`;
 
     fetch( url )
         .then(function(response) {
@@ -62,6 +63,7 @@ function oneCall(lat,lon) {
         .then(function(data) {
 
             // console.log( data );
+            
 
             // Render to page.
             fiveDay(data);
@@ -69,15 +71,42 @@ function oneCall(lat,lon) {
     
 }
 
+const cardTemp = document.getElementById(".temp");
+const cardWind = document.getElementById(".wind");
+const cardHum = document.getElementById(".hum");
+const cardIcon = document.getElementById(".icon");
+const cardDate = document.getElementById(".date");
+
     function fiveDay(data) {
         console.log(data.daily);
         for (let i = 0; i < 4; i++) {
-            const element = data.daily[i].temp.day;
-            console.log(element);
+            const temp = data.daily[i].temp.day;
+            const wind = data.daily[i].wind_speed;
+            const hum = data.daily[i].humidity;
+            const icon = data.daily[i].weather[0].icon;
+            const date = data.daily[i].temp.day;
+            console.log(temp, wind, hum, icon, date)
+            var fer = kelvinConvert(temp);
+            console.log(fer)
+            // append to html
+            cardTemp.appendChild(temp);
+            cardWind.textContent = wind;
+            cardHum.textContent = hum;
+            cardIcon.textContent = icon;
+            cardDate.textContent = date;
+
         }
         
     }
 
+    function kelvinConvert(temp) {
+        valNum = ((temp-273.15)*1.8)+32;
+        console.log(valNum)
+        return valNum;
+        
+    }
+
+    // ((valNum-273.15)*1.8)+32
     // lat
     
     // lon
