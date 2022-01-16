c15079d809c255758368d56b1ba577317c1ad55b
Bertokeys1 <abarrientos93.ab@gmail.com>
2022-01-16 15:16:58 -0800

    added comments to html,css,js files. also updated readme file.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # weather-dashboard-
+
+This is a 5 day weather dashboard!
+
+Just search a city and you will see current weather information, and for the next 5 days. 
+
+This app was built using HTML, CSS, and javascript with a weather API from openweatherapp.com.
+
+What to expect when using the app:
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+-git hub repo <https://github.com/Bertokeys1/weather-dashboard>
+-live page <https://bertokeys1.github.io/weather-dashboard/>
+
+
+
+![img](Screenshot.png)
+![img](Screenshot2.png)
--- assests/script.js
+++ assests/script.js
@@ -1,26 +1,18 @@
-// Print/render the weather data to the page.
-
-// From the <form> element, listen to the "submit"
-// From the <button> element, listen to the "click"
-
-    // Select <input>, get its value, and provide it to the geo API
-
-// From the <button> container element, listen to the <button> "click"
-
-    // Get the vity from the button's data attribute
-
-
 var currentWeather = $("#currentWeather")
 var searchBtn = $("#search-btn")
 var fiveDayEl = $("#fiveDay")
 var cityList = [];
 var historyEl = $("#history")
+
+// Gets searches saved in local storage as an array.
 if (localStorage.getItem("cityList")) {
     cityList = JSON.parse(localStorage.getItem("cityList"))
 }
 
+// Search button function.
 searchBtn.on("click", cityName);
 
+// Displays history of searches of citys.
 function displayCity() {
     historyEl.empty()
     for (let i = 0; i < cityList.length; i++) {
@@ -31,30 +23,29 @@ function displayCity() {
         geoData($(this).text())
     })
 }
+
+// Fuction to get location from user search
 displayCity()
 function cityName() {
     var location = $("#search").val();
     geoData(location);
 }
 
-// Fetch the geo data (lat, lon)
+// Function to get geo data for search location.
 function geoData(cityName) {
-
-    //build url
     var APIKey = "5f0de267b85fd710a6d2a256aad58f56";
     var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
-    // var url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5f0de267b85fd710a6d2a256aad58f56`;
 
     fetch( weatherUrl )
+
         .then(function(response) {
             return response.json();
         })
+
         .then(function(data) {
 
             var lat = data.city.coord.lat;
             var lon = data.city.coord.lon; 
-
-            // console.log(lat, lon);
 
             oneCall( lat, lon, data)
 
@@ -62,13 +53,7 @@ function geoData(cityName) {
 
 }
 
-    // q = Name of the city
-
-    // limit = 5 (optional)
-
-    // appid = Your custome API key
-
-// Fetch the one call weather data
+// Funtion to fetch data and display the current day card on webpage.
 function oneCall(lat,lon, currentData) {
 
     var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5f0de267b85fd710a6d2a256aad58f56&units=imperial`;
@@ -80,7 +65,8 @@ function oneCall(lat,lon, currentData) {
         .then(function(data) {
             console.log(currentData)
             console.log( data );
-            currentWeather.html(`            <div class="city">city: ${currentData.city.name} ${moment(data.current.dt,"X").format("MM/DD/YYYY")} <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png"></div>
+            currentWeather.html(`            
+            <div class="city">city: ${currentData.city.name} ${moment(data.current.dt,"X").format("MM/DD/YYYY")} <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png"></div>
             <div class="topTemp">temp: ${data.current.temp}</div>
             <div class="topwind">wind speed: ${data.current.wind_speed}</div>
             <div class="tophum">Humidity:${data.current.humidity}</div>
@@ -91,7 +77,6 @@ function oneCall(lat,lon, currentData) {
                 localStorage.setItem("cityList", JSON.stringify(cityList))
             }
             displayCity()
-            // Render to page.
             fiveDay(data);
         });
     
@@ -103,20 +88,20 @@ const cardHum = document.getElementById("hum");
 const cardIcon = document.getElementById("icon");
 const cardDate = document.getElementById("date");
 
-    function fiveDay(data) {
-        console.log(data.daily);
-        fiveDayEl.empty()
-        for (let i = 1; i < 6; i++) {
-            const temp = data.daily[i].temp.day;
-            const wind = data.daily[i].wind_speed;
-            const hum = data.daily[i].humidity;
-            const icon = data.daily[i].weather[0].icon;
-            const date = data.daily[i].temp.day;
-            console.log(temp, wind, hum, icon, date)
-            var fer = kelvinConvert(temp);
-            console.log(fer)
-            // append to html
-            fiveDayEl.append(`
+// Function to get data for 5 day weather cards.
+function fiveDay(data) {
+    console.log(data.daily);
+    fiveDayEl.empty()
+    for (let i = 1; i < 6; i++) {
+        const temp = data.daily[i].temp.day;
+        const wind = data.daily[i].wind_speed;
+        const hum = data.daily[i].humidity;
+        const icon = data.daily[i].weather[0].icon;
+        const date = data.daily[i].temp.day;
+        console.log(temp, wind, hum, icon, date)
+        var fer = kelvinConvert(temp);
+        console.log(fer)
+        fiveDayEl.append(`
             <div class="weather card col-md m-2">
             <div class="card-body">
               <div class="date">Date: ${moment(data.daily[i].dt,"X").format("MM/DD/YYYY")}</div>
@@ -127,30 +112,16 @@ const cardDate = document.getElementById("date");
             </div>
           </div>
             
-            `)
+        `)
 
-        }
-        
     }
-
-    function kelvinConvert(temp) {
-        valNum = ((temp-273.15)*1.8)+32;
-        console.log(valNum)
-        return valNum;
         
-    }
+}
 
-    // ((valNum-273.15)*1.8)+32
-    // lat
-    
-    // lon
-
-    // appid
-
-    // units = imperial
-
-    // exclude = minutely, hourly
-
-
-
-// Print/render the weather data to the page.
+// Function to conver kelvin temp to fahrenheit. 
+function kelvinConvert(temp) {
+    valNum = ((temp-273.15)*1.8)+32;
+    console.log(valNum)
+    return valNum;
+        
+}
